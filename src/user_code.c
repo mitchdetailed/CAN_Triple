100e8b86d3669ffaf169218fdddd0cd1126044c2
Mitch Minton <mitchminton@gmail.com>
2024-03-31 22:33:43 -0500

    update send_message function

--- src/user_code.c
+++ src/user_code.c
@@ -10,31 +10,37 @@
 /* End File Includes */
 
 /* Variable Declarations */
-uint32_t serialnumber;
 uint8_t example_data_1Hz[8] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
-uint8_t rdp_Level = 0;
-uint32_t flashbaseaddr = 0x0800F800; // Start Address of last page of memory
+uint32_t sn1 = 0;
+uint32_t sn2 = 0;
+uint32_t sn3 = 0;
+//uint8_t rdp_Level = 0;
+//uint32_t flashbaseaddr = 0x0800F800; // Start Address of last page of memory
 
 /* End Variable Declarations */
 
 /* Startup Functions */
 void events_Startup(){
-	setCANBitrate(CAN_1, 100000);
-	setCANBitrate(CAN_2, 100000);
-	//setCANBitrate(CAN_3, 100000);
+	setCANBitrate(CAN_1, 1000000);
+	setCANBitrate(CAN_2, 1000000);
+	setCANBitrate(CAN_3, 1000000);
 	setCAN_Termination(CAN_1,true);
 	setCAN_Termination(CAN_2, true);
-	//setCAN_Termination(CAN_3, true);
-	startCANbus(CAN_1);
-	startCANbus(CAN_2);
+	setCAN_Termination(CAN_3, true);
+	//startCANbus(CAN_1);
+	//startCANbus(CAN_2);
 	//startCANbus(CAN_3);
+	sn1 = getSerialNumber1of3();
+	sn2 = getSerialNumber2of3();
+	sn3 = getSerialNumber3of3();
+
 }
 /* End Startup Functions */
 
 /* When a CAN Message is Received.... */
 void onReceive(CAN_Message Message){
 	if (Message.Bus == CAN_1){
-		add_to_CAN_TX_Queue(CAN_2, Message.is_extended_id, Message.arbitration_id, Message.dlc, Message.data);
+		send_message(CAN_2, Message.is_extended_id, Message.arbitration_id, Message.dlc, Message.data);
 	}
 	if (Message.Bus == CAN_2){
 		// Do nothing
@@ -83,7 +89,7 @@ void events_20Hz(){
 
 /* Run 10Hz Functions here */
 void events_10Hz(){
-	
+	toggleLED(LED_1);
 }
 
 /* Run 5Hz Functions here */
@@ -93,15 +99,15 @@ void events_5Hz(){
 
 /* Run 2Hz Functions here */
 void events_2Hz(){
-	toggleLED(LED_1);
+	
 }
 
 /* Run 1Hz Functions here */
 void events_1Hz(){
-	add_to_CAN_TX_Queue(CAN_1, false, 0x001, 8, example_data_1Hz);
-	add_to_CAN_TX_Queue(CAN_2, false, 0x002, 8, example_data_1Hz);
-	add_to_CAN_TX_Queue(CAN_3, false, 0x003, 8, example_data_1Hz);
-	for (uint8_t i=0; i<8; i++){
-		example_data_1Hz[i]++;
-	}
+	//send_message(CAN_1, false, 0x001, 8, example_data_1Hz);
+	//send_message(CAN_2, false, 0x002, 8, example_data_1Hz);
+	//send_message(CAN_3, false, 0x003, 8, example_data_1Hz);
+	//for (uint8_t i=0; i<8; i++){
+	//	example_data_1Hz[i]++;
+	//}
 }
