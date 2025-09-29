fd532c310f3bc657fcd54c4096a5f5f69d82aea6
Mitch Minton <mitchminton@gmail.com>
2025-09-28 19:48:32 -0500

    updated reset handler for faults

--- Projects/Haltech Devices/Haltech Keypad 2x4 B Emulation/src/stm32g4xx_it.c
+++ Projects/Haltech Devices/Haltech Keypad 2x4 B Emulation/src/stm32g4xx_it.c
@@ -79,9 +79,8 @@ void NMI_Handler(void)
 
   /* USER CODE END NonMaskableInt_IRQn 0 */
   /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
-  while (1)
-  {
-  }
+  // Perform system reset instead of infinite loop
+  NVIC_SystemReset();
   /* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
@@ -93,11 +92,8 @@ void HardFault_Handler(void)
   /* USER CODE BEGIN HardFault_IRQn 0 */
 
   /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
-    /* USER CODE END W1_HardFault_IRQn 0 */
-  }
+  // Perform system reset instead of infinite loop
+  NVIC_SystemReset();
 }
 
 /**
@@ -108,11 +104,8 @@ void MemManage_Handler(void)
   /* USER CODE BEGIN MemoryManagement_IRQn 0 */
 
   /* USER CODE END MemoryManagement_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
-    /* USER CODE END W1_MemoryManagement_IRQn 0 */
-  }
+  // Perform system reset instead of infinite loop
+  NVIC_SystemReset();
 }
 
 /**
@@ -123,11 +116,8 @@ void BusFault_Handler(void)
   /* USER CODE BEGIN BusFault_IRQn 0 */
 
   /* USER CODE END BusFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
-    /* USER CODE END W1_BusFault_IRQn 0 */
-  }
+  // Perform system reset instead of infinite loop
+  NVIC_SystemReset();
 }
 
 /**
@@ -138,11 +128,8 @@ void UsageFault_Handler(void)
   /* USER CODE BEGIN UsageFault_IRQn 0 */
 
   /* USER CODE END UsageFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
-    /* USER CODE END W1_UsageFault_IRQn 0 */
-  }
+  // Perform system reset instead of infinite loop
+  NVIC_SystemReset();
 }
 
 /**
